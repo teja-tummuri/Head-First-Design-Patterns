24db31bbf07ab2bb367426284165d56567241364
Teja Srinivas Ganesh Tummuri <tejaganesh94@gmail.com>
2017-07-29 17:02:42 -0700

    Demo Swing Application
    TO get a hang of actions and listeners in Swing

--- src/com/gani/observer/swing/SwingObserverExample.java
+++ src/com/gani/observer/swing/SwingObserverExample.java
@@ -1,8 +1,39 @@
 package com.gani.observer.swing;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by Gani on 7/29/17.
  */
 public class SwingObserverExample {
-    
+
+    JFrame frame;
+    public static void main(String[] args){
+        SwingObserverExample example = new SwingObserverExample();
+        example.go();
+    }
+
+    public void go(){
+        frame = new JFrame();
+
+        JButton button = new JButton("Should I do it?");
+        button.addActionListener(new AngelListener());
+        button.addActionListener(e -> System.out.println("Do it, there is nothing to lose!!"));
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.CENTER, button);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+
+    class AngelListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Don't do it, you might regret it later!!");
+        }
+    }
 }
