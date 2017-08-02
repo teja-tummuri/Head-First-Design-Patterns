16d02df0cf692d75ed9b5dedea9ca483507db511
Teja Srinivas Ganesh Tummuri <tejaganesh94@gmail.com>
2017-08-02 14:22:02 -0700

    Template method
    Created tea and coffee classes with redundancies

---
+++ src/com/gani/template/Coffee.java
@@ -0,0 +1,30 @@
+package com.gani.template;
+
+/**
+ * Created by Gani on 8/2/17.
+ */
+public class Coffee {
+
+    void prepareRecipe(){
+        boilWater();
+        brewCoffee();
+        pourToCup();
+        addSugarAndMilk();
+    }
+
+    public void boilWater(){
+
+    }
+
+    public void brewCoffee(){
+
+    }
+
+    public void pourToCup(){
+
+    }
+
+    public void addSugarAndMilk(){
+
+    }
+}
---
+++ src/com/gani/template/Tea.java
@@ -0,0 +1,32 @@
+package com.gani.template;
+
+/**
+ * Created by Gani on 8/2/17.
+ */
+public class Tea {
+
+    void prepareRecipe(){
+        boilWater();
+        steepTea();
+        pourToCup();
+        addLemon();
+    }
+
+    public void boilWater(){
+
+    }
+
+    public void steepTea(){
+
+    }
+
+    public void pourToCup(){
+
+    }
+
+    public void addLemon(){
+
+    }
+
+}
+
