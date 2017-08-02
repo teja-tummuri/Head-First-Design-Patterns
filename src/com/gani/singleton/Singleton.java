ae1fd686b4cce1f50b26573024281f79da5b8389
Teja Srinivas Ganesh Tummuri <tejaganesh94@gmail.com>
2017-08-01 18:06:47 -0700

    Singleton Pattern
    # The SIngleton Pattern ensures a class has only one instance, and provides a global point of access to it.

---
+++ src/com/gani/singleton/Singleton.java
@@ -0,0 +1,19 @@
+package com.gani.singleton;
+
+/**
+ * Created by Gani on 8/1/17.
+ */
+public class Singleton {
+
+    private static Singleton uniqueInstance;
+
+    private Singleton(){}
+
+    public static Singleton getInstance(){
+
+        if(uniqueInstance == null)
+            uniqueInstance = new Singleton();
+
+        return uniqueInstance;
+    }
+}
