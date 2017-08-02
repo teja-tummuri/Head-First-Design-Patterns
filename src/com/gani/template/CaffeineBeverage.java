848390271d8d65ed73ad9ef53f1b7d39829667f3
Teja Srinivas Ganesh Tummuri <tejaganesh94@gmail.com>
2017-08-02 14:25:05 -0700

    Added CaffeineBeverage super class
    Redesigned to abstract the common methods

---
+++ src/com/gani/template/CaffeineBeverage.java
@@ -0,0 +1,11 @@
+package com.gani.template;
+
+/**
+ * Created by Gani on 8/2/17.
+ */
+public abstract class CaffeineBeverage {
+
+    abstract void prepareRecipe();
+    abstract void boilWater();
+    abstract void pourToCup();
+}
